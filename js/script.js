dbac07d83af81fbf56897d4213e4528a1ac59219
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2021-11-12 21:44:57 +0300

    upload

---
+++ js/script.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const init = () => {
+
+};
+
+init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+'use strict';
+
+const buttonThemeLight = document.querySelector('.theme-button-light');
+const buttonThemeDark = document.querySelector('.theme-button-dark');
+
+const buttonfontSansSerif = document.querySelector('.font-button-sans-serif');
+const buttonfontSerif = document.querySelector('.font-button-serif');
+
+const buttonfontSmall = document.querySelector('.font-size-button-s');
+const buttonfontMedium = document.querySelector('.font-size-button-m');
+const buttonfontLarge = document.querySelector('.font-size-button-l');
+
+
+  const buttonThemeLightHandler = () => {
+    document.documentElement.dataset['themeName'] = 'light';
+  };
+  
+  const buttonThemeDarkHandler = () => {
+    document.documentElement.dataset['themeName'] = 'dark';
+  };
+  
+
+  const buttonfontSansSerifHandler = () => {
+    document.documentElement.dataset['themeFont'] = 'sans serif';
+  };
+  
+  const buttonfontSerifHandler = () => {
+    document.documentElement.dataset['themeFont'] = 'serif';
+  };
+
+
+  const buttonFontSmallHandler = () => {
+    document.documentElement.dataset['themeFontSize'] = 'small';
+  };
+  
+  const buttonFontMediumHandler = () => {
+    document.documentElement.dataset['themeFontSize'] = 'medium';
+  };
+  
+  const buttonFontLargeHandler = () => {
+    document.documentElement.dataset['themeFontSize'] = 'large';
+  };
+
+
+
+
+  const init = () => {
+    buttonThemeLight.addEventListener('click', buttonThemeLightHandler);
+    buttonThemeDark.addEventListener('click', buttonThemeDarkHandler);
+
+    buttonfontSansSerif.addEventListener('click', buttonfontSansSerifHandler);
+    buttonfontSerif.addEventListener('click', buttonfontSerifHandler);
+
+    buttonfontSmall.addEventListener('click', buttonFontSmallHandler);
+    buttonfontMedium.addEventListener('click', buttonFontMediumHandler);
+    buttonfontLarge.addEventListener('click', buttonFontLargeHandler);
+  };
+  
+  init();
+  */
