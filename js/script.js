d27b74175d8bde8668318f0e04f5c12e5e846f0d
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2021-11-13 13:42:14 +0300

    delete code for classes and eslint validator

--- js/script.js
+++ js/script.js
@@ -1,104 +1,75 @@
 'use strict';
 
 const SettingType = {
-    ATTRIBUTE: 'attribute',
-    CLASS: 'class',
+  ATTRIBUTE: 'attribute',
 };
 
-const containers = document.querySelectorAll('.js-buttons-container');
-
+const container = document.querySelector('.js-buttons-container');
 const settingButtons = document.querySelectorAll('[data-setting-name]');
 
 const setDataAttribute = ({settingTarget}, params) => {
-    const element = document.querySelector(settingTarget);
-    for (const [key, value] of Object.entries(params)) {
-      element.dataset[key] = value;
-    }
-  };
+  const element = document.querySelector(settingTarget);
 
-const setClass = ({settingTarget}, params) => {
-    const element = document.querySelector(settingTarget);
-
-    for (const [key, value] of Object.entries(params)) {
-        const elements = Array.from(settingButtons)
-            .filter((element) => element.dataset['settingName'] === key)
-        // удалить классы, название которых совпадает со значением
-        // атрибута setting-value элементов с setting-name равным key
-        elements.forEach((item) => element.classList.remove(item.dataset.settingValue));
-
-        element.classList.add(value);
-    }
+  for (const [key, value] of Object.entries(params)) {
+    element.dataset[key] = value;
+  }
 };
-  
+
 const setButtonActive = (params) => {
-    for (const [key, value] of Object.entries(params)) {
-        // находим активную кнопку для настройки
-        const activeButton = Array.from(settingButtons)
+  for (const [key, value] of Object.entries(params)) {
+    // находим активную кнопку для настройки
+    const activeButton = Array.from(settingButtons)
             .find((element) => element.dataset['settingName'] === key && element.classList.contains('active'));
 
-        // находим кнопку, которую устанавливаем для настройки
-        const setButton = Array.from(settingButtons)
+    // находим кнопку, которую устанавливаем для настройки
+    const setButton = Array.from(settingButtons)
             .find((element) => element.dataset['settingName'] === key && element.dataset['settingValue'] === value);
 
-        // снимаем класс active с кнопки, которая ранее была активной
-        activeButton.classList.remove('active');
+    // снимаем класс active с кнопки, которая ранее была активной
+    activeButton.classList.remove('active');
 
-        // добавляем класс active с кнопки, которую устанавливаем
-        setButton.classList.add('active');
-    }
+    // добавляем класс active с кнопки, которую устанавливаем
+    setButton.classList.add('active');
+  }
 };
 
 
 const applySetting = (setting, params) => {
-    if (setting.settingType === SettingType.CLASS) {
-        setClass(setting, params);
-    } else if (setting.settingType === SettingType.ATTRIBUTE) {
-        setDataAttribute(setting, params);
-    }
+   if (setting.settingType === SettingType.ATTRIBUTE) {
+    setDataAttribute(setting, params);
+  }
 
-    setButtonActive(params);
-  };
-  
+  setButtonActive(params);
+};
+
 const settingButtonClickHandler = (evt, setting) => {
   const button = evt.target.closest('button');
 
-  if (!button) {
+  if (!button || button.classList.contains('active')) {
     return;
   }
 
   const params = {};
 
-  const{settingName, settingValue} = button.dataset;
+  const {settingName, settingValue} = button.dataset;
   params[settingName] = settingValue;
-  
+
   applySetting(setting, params);
 };
 
 const init = () => {
-    containers.forEach((container) => {
-        const setting = container.dataset;
-    
-        container.addEventListener('click', (evt) => {
-          settingButtonClickHandler(evt, setting);
-        });
+ 
+    const setting = container.dataset;
+
+    container.addEventListener('click', (evt) => {
+      settingButtonClickHandler(evt, setting);
     });
 };
 
 init();
 
-
-
-
-
-
-
-
-
-
-
-
-
 /*
+
 тот же самый код до рефакторинга
 
 'use strict';
@@ -117,16 +88,15 @@ const buttonfontLarge = document.querySelector('.font-size-button-l');
   const buttonThemeLightHandler = () => {
     document.documentElement.dataset['themeName'] = 'light';
   };
-  
+
   const buttonThemeDarkHandler = () => {
     document.documentElement.dataset['themeName'] = 'dark';
   };
-  
 
   const buttonfontSansSerifHandler = () => {
     document.documentElement.dataset['themeFont'] = 'sans serif';
   };
-  
+
   const buttonfontSerifHandler = () => {
     document.documentElement.dataset['themeFont'] = 'serif';
   };
@@ -135,17 +105,14 @@ const buttonfontLarge = document.querySelector('.font-size-button-l');
   const buttonFontSmallHandler = () => {
     document.documentElement.dataset['themeFontSize'] = 'small';
   };
-  
+
   const buttonFontMediumHandler = () => {
     document.documentElement.dataset['themeFontSize'] = 'medium';
   };
-  
+
   const buttonFontLargeHandler = () => {
     document.documentElement.dataset['themeFontSize'] = 'large';
   };
-
-
-
 
   const init = () => {
     buttonThemeLight.addEventListener('click', buttonThemeLightHandler);
@@ -158,6 +125,6 @@ const buttonfontLarge = document.querySelector('.font-size-button-l');
     buttonfontMedium.addEventListener('click', buttonFontMediumHandler);
     buttonfontLarge.addEventListener('click', buttonFontLargeHandler);
   };
-  
+
   init();
-  */
+*/
